f00bb463cf8d2009f608bc46aa642d8aa892f577
Martin89899 <mart.vergaral@duocuc.cl>
2025-05-13 09:12:50 -0400

    first commit

---
+++ src/test/java/com/progreso/Progreso/ProgresoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.progreso.Progreso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgresoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
